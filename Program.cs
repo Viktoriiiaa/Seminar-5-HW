e46570a8c27eccbc8cc42365731d57ab3d4c3593
Viktoriiiaa <115409499+Viktoriiiaa@users.noreply.github.com>
2022-12-01 21:20:41 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+
+Console.WriteLine("Введите длину массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for(int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+int count = 0;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] % 2 == 0)
+count++;
+}
+Console.WriteLine($"из {numbers.Length} чисел, {count} четных");
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+*/
+
+
+//Задайте массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях. 
+/* 
+
+Console.WriteLine($"Введи длину массива: ");
+int ElementNumbers = Convert.ToInt32(Console.ReadLine()); 
+int randomNumber =  RandomNumbers(ElementNumbers, 1, 10);
+Console.WriteLine($"  Сумма элементов, стоящих на нечётных позициях: {randomNumber}");
+
+int RandomNumbers(int ElementNumbers, int min, int max)
+  {
+  int[] randomNumber = new int[ElementNumbers];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i <randomNumber.Length; i++ ){
+      randomNumber[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumber[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumber[i];
+      }
+    }
+  return sumElements;
+  }
+  */
+
+  //Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+/*
+
+Console.WriteLine("Введите длину массива  ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] number = new double[size];
+
+FillArrayRandomNumbers(number);
+
+Console.WriteLine("Массив: ");
+
+PrintArray(number);
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int a = 0; a < number.Length; a++)
+{
+    if (number[a] > max)
+        {
+            max = number[a];
+        }
+    if (number[a] < min)
+        {
+            min = number[a];
+        }
+}
+
+Console.WriteLine($"Всего в массиве {number.Length} чисел. Максимальное = {max}, Минимальное = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным = {max - min}");
+
+void FillArrayRandomNumbers(double[] number)
+{
+    for(int i = 0; i < number.Length; i++)
+        {
+            number[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] number)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < number.Length; i++)
+        {
+            Console.Write(number[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+*/
